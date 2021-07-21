02bb719b027f818dd63fb9952000a42505c837d8
Ángel Faraldo <angelfaraldo@gmail.com>
2021-07-21 21:09:39 +0200

    added filename in file

--- 2-02_condicionales-y-loops.py
+++ 2-02_condicionales-y-loops.py
@@ -3,8 +3,7 @@ INTRODUCCIÓN A LA PROGRAMACIÓN EN PYTHON A TRAVÉS DE LA MÚSICA
 Ángel Faraldo, del 19 al 23 de julio de 2021
 Campus Junior, Universitat Pompeu Fabra
 
-"nombre-del-archivo"
+"2-02 Condicionales-y-loops"
 contenidos:
 """
-
 
